d2208f7b7afec1302a89205fb3080473371109a1
jimmyblackwood576@outlook.com <jimmyblackwood576@outlook.com>
2022-08-12 23:57:23 +0300

    Дополнение задания

--- Function_and_Recursion/Function_and_Recursion.cpp
+++ Function_and_Recursion/Function_and_Recursion.cpp
@@ -16,7 +16,37 @@ void sortString(int arr[4][4]);
 
 void sortString(double arr[4][4]);
 
+int NOD(int a, int b);
 
+void create_arr(int arr[]) {
+	for (int i = 0; i < 4; i++) {
+		arr[i] = 1 + rand() % 9;
+	}
+}
+
+void print_arr(int arr[]) {
+	for (int i = 0; i < 4; i++) {
+		cout<<arr[i]<<" ";
+	}
+}
+
+void enter_arr(int arr[]) {
+	int n;
+	cout << "Enter single digit numbers one by one: \n";
+	for (int i = 0; i < 4; i++) {
+		cin >> n;
+		arr[i] = n;
+	}
+}
+ 
+void comparison(int arr[], int arr1[]) {
+	for (int i = 0; i < 4; i++) {
+		if (arr[i] != arr1[i])
+			cout << '*' << " ";
+		else if (arr[i] == arr1[i])
+			cout << arr[i] << " ";
+	}
+}
 
 int main()
 {
@@ -36,7 +66,7 @@ int main()
 		{2,3,6,5},
 		{4,2,3,6},
 		{2,1,1,5}};
-	
+	cout << "Task 1 \n\n";
 	printarr(arr);
 	printarr(arr1);
 	printarr(arr2);
@@ -44,9 +74,30 @@ int main()
 	MinAndMax(arr1);
 	sortString(arr);
 	sortString(arr1);
+	system("pause");
+	system("cls");
 	//Task 2___________________________
+	cout << "Task 2 \n\n";
+	int a, b;
+	cout << "Enter first number: ";
+	cin >> a;
+	cout << "Enter second number: ";
+	cin >> b;
+	cout<<" NOD = " << NOD(a, b) <<endl;
+	system("pause");
+	system("cls");
+	//Task 3___________________________
+	int cow[4];
+	int bull[4];
+	cout << "Task 3 \n\n";
+	create_arr(cow);
+	print_arr(cow);
+	enter_arr(bull);
+	comparison(cow, bull);
 
 }
+
+
 
 
 void printarr(int arr[4][4])
@@ -141,4 +192,13 @@ void sortString(double arr[4][4]) {
 		}
 		cout << "\n";
 	}
+}
+
+int NOD(int a, int b) {
+	if (b == 0)
+		return a;
+	if (a > b)
+		return NOD(b, a % b);
+	else 
+		return NOD(a, b % a);
 }
